7b7ba2ac50fd656fe87fea604cb339256f2533e0
JR Shampang <jr@shampang.com>
2017-01-05 22:19:38 -0600

    Add rollup-babel-react

---
+++ rollup-babel-react/rollup.config.js
@@ -0,0 +1,27 @@
+import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import replace from 'rollup-plugin-replace'
+
+export default {
+	entry: 'index.js',
+	dest: 'bundle.js',
+	format: 'iife',
+	plugins: [
+		replace({
+			'process.env.NODE_ENV': JSON.stringify('production')
+		}),
+		nodeResolve({
+			jsnext: true
+		}),
+		commonjs({
+			include: 'node_modules/**',
+			namedExports: {
+				'react-dom': ['render']
+			}
+		}),
+		babel({
+			exclude: 'node_modules/**'
+		})
+	]
+}
