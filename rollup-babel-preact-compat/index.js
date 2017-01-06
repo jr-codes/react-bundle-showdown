c85f871cb487d0e53eb5591b451f64e34203170e
ezekielchentnik <zeek@ezekielchentnik.com>
2017-01-05 22:51:27 -0600

    fix preact-compat alias

--- rollup-babel-preact-compat/index.js
+++ rollup-babel-preact-compat/index.js
@@ -1,9 +1,5 @@
 /* global document */
-// import { h, render } from 'preact'
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-import { Provider } from 'preact-redux'
+import React from 'react'
+import { render } from 'react-dom'
 
-console.log(Provider)
-
-// ReactDOM.render(<div>Hello World</div>, document.body)
+render(<div>Hello World</div>, document.body)
--- rollup-babel-preact-compat/rollup.config.js
+++ rollup-babel-preact-compat/rollup.config.js
@@ -1,14 +1,19 @@
 import babel from 'rollup-plugin-babel'
-import alias from 'rollup-plugin-alias'
+import alias from 'rollup-plugin-strict-alias'
 import commonjs from 'rollup-plugin-commonjs'
 import nodeResolve from 'rollup-plugin-node-resolve'
 import replace from 'rollup-plugin-replace'
+import path from 'path'
 
 export default {
 	entry: 'index.js',
 	dest: 'bundle.js',
 	format: 'iife',
 	plugins: [
+		alias({
+			'react': path.resolve('./node_modules/preact-compat/src/index.js'),
+			'react-dom': path.resolve('./node_modules/preact-compat/src/index.js')
+		}),
 		replace({
 			'process.env.NODE_ENV': JSON.stringify('production')
 		}),
