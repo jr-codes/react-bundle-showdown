223fa880f86ebb74cf8621869caa0f45a902c075
JR Shampang <jr@shampang.com>
2017-01-05 22:20:06 -0600

    Add rollup-babel-preact-compat

---
+++ rollup-babel-preact-compat/index.js
@@ -0,0 +1,9 @@
+/* global document */
+// import { h, render } from 'preact'
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+import { Provider } from 'preact-redux'
+
+console.log(Provider)
+
+// ReactDOM.render(<div>Hello World</div>, document.body)
---
+++ rollup-babel-preact-compat/rollup.config.js
@@ -0,0 +1,26 @@
+import babel from 'rollup-plugin-babel'
+import alias from 'rollup-plugin-alias'
+import commonjs from 'rollup-plugin-commonjs'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import replace from 'rollup-plugin-replace'
+
+export default {
+	entry: 'index.js',
+	dest: 'bundle.js',
+	format: 'iife',
+	plugins: [
+		replace({
+			'process.env.NODE_ENV': JSON.stringify('production')
+		}),
+		nodeResolve({
+			jsnext: true
+		}),
+		commonjs({
+			include: 'node_modules/**',
+			namedExports: { 'preact-redux': ['connect', 'Provider' ]}
+		}),
+		babel({
+			exclude: 'node_modules/**'
+		})
+	]
+}
