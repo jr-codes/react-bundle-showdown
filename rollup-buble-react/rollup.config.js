e2da20ae4f6c042e36a3b6d05a83b5de267296ce
JR Shampang <jr@shampang.com>
2017-01-14 19:55:10 -0600

    Add rollup-buble-react

---
+++ rollup-buble-react/rollup.config.js
@@ -0,0 +1,25 @@
+import buble from 'rollup-plugin-buble'
+import commonjs from 'rollup-plugin-commonjs'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import replace from 'rollup-plugin-replace'
+
+export default {
+  entry: 'index.js',
+  dest: 'bundle.js',
+  format: 'iife',
+  plugins: [
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    nodeResolve({
+      jsnext: true
+    }),
+    commonjs({
+      include: 'node_modules/**',
+      namedExports: {
+        'react-dom': ['render']
+      }
+    }),
+    buble()
+  ]
+}
