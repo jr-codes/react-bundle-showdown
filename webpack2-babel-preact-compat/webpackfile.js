eda6d7a6f54b8bcee7d3c4592d79be91c0ff9d56
JR Shampang <jr@shampang.com>
2017-01-14 18:03:53 -0600

    Add webpack2-babel-preact-compat

---
+++ webpack2-babel-preact-compat/webpackfile.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ['babel-loader'],
+        exclude: [/node_modules/]
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'react': 'preact-compat',
+      'react-dom': 'preact-compat'
+    }
+  }
+}
