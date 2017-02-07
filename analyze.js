bb5ad78fb1817c65df5f93856cf1df728fea8175
ezekielchentnik <zeek@ezekielchentnik.com>
2017-02-07 11:01:18 -0600

    push up current standalone selenium

--- analyze.js
+++ analyze.js
@@ -1,1 +1,33 @@
-console.log('running analyze')
+var selenium = require('selenium-standalone');
+
+selenium.install({
+  // check for more recent versions of selenium here:
+  // https://selenium-release.storage.googleapis.com/index.html
+  version: '3.0.1',
+  baseURL: 'https://selenium-release.storage.googleapis.com',
+  drivers: {
+    chrome: {
+      // check for more recent versions of chrome driver here:
+      // https://chromedriver.storage.googleapis.com/index.html
+      version: '2.27',
+      arch: process.arch,
+      baseURL: 'https://chromedriver.storage.googleapis.com'
+    }
+  },
+  logger: function(message) {
+// console.log(message);
+  },
+  progressCb: function(totalLength, progressLength, chunkLength) {
+
+  }
+}, function(err) {
+  console.log('in callback');
+
+
+  selenium.start(function (err, child) {
+    console.log(err)
+  });
+
+
+
+});
