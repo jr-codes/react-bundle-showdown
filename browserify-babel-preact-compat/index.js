f6817f812742b32b616d7d139a6ce6c1a2edfde2
JR Shampang <jr@shampang.com>
2017-09-20 00:03:30 -0500

    Remove compat projects and webpack1 projects
    To narrow the focus of this project, let's ignore the compat scenarios and just focus on using preact/inferno/react-lite natively. Also, let's stick with the latest version of each bundler, so get rid of old webpack.

--- browserify-babel-preact-compat/index.js
+++
@@ -1,5 +0,0 @@
-/* global document */
-import React from 'react'
-import { render } from 'react-dom'
-
-render(<div>Hello World</div>, document.body)
--- webpack-babel-preact/index.js
+++
@@ -1,5 +0,0 @@
-/** @jsx h */
-/* global document */
-import { h, render } from 'preact'
-
-render(<div>Hello World</div>, document.body)
